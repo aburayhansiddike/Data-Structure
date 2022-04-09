527d2477f7407fa25653cba717d143d4880cda52
rayhan <aburayhansiddike@gmail.com>
2022-04-09 12:26:59 +0600

    Find the second smallest element of an unsorted array is compelete

---
+++ second_smallest_element_of_an_unsorted_array.c
@@ -0,0 +1,47 @@
+#include<stdio.h>
+
+
+int FindSecondSmallestElement(int a[] , int n){
+
+       if(n < 2){
+             return -1 ;
+       }
+       int firstSmallElement = 10000000 , secondSmallElement = 10000000 ;
+
+       for(int i = 0 ;i < n ; i += 1){
+             if(a[i] < firstSmallElement){
+                   firstSmallElement = a[i];
+             }
+       }
+
+       for(int i = 0 ; i < n ; i += 1){
+            if(a[i] > firstSmallElement && a[i] < secondSmallElement){
+                 secondSmallElement = a[i];
+            }
+       }
+       
+       return secondSmallElement ;
+       
+}
+int main(){
+      int n , a[1000] ;
+      printf("Number of elements of the array : ");
+
+      scanf("%d" , &n);
+
+      printf("Elements of the array : ");
+      
+      for(int  i = 0 ; i < n ; i += 1){
+            scanf("%d" , &a[i]);
+      }
+
+      int secondSmallestElement = FindSecondSmallestElement(a , n);
+
+      if(secondSmallestElement == -1){
+           printf("The input is not valid\n");
+      }else{
+            printf("The second largest element is : %d\n" , secondSmallestElement);
+      }
+
+      return 0 ;
+}
