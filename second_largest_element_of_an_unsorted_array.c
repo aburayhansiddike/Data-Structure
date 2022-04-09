d834ec227c6be9f4e152754a5183a42169de26d8
rayhan <aburayhansiddike@gmail.com>
2022-04-09 12:17:48 +0600

    Find the second largest element of the array is complete

---
+++ second_largest_element_of_an_unsorted_array.c
@@ -0,0 +1,47 @@
+#include<stdio.h>
+
+
+int FindSecondLargestElement(int a[] , int n){
+
+       if(n < 2){
+             return -1 ;
+       }
+       int firstMaxElement = 0 , secondMaxElement = 0 ;
+
+       for(int i = 0 ;i < n ; i += 1){
+              if(a[i] > firstMaxElement){
+                    firstMaxElement = a[i];
+              }
+       }
+
+       for(int i = 0 ; i < n ;  i += 1){
+             if(a[i] > secondMaxElement && a[i] < firstMaxElement){
+                    secondMaxElement = a[i];
+             }
+       }
+
+       return secondMaxElement ;
+       
+}
+int main(){
+      int n , a[1000] ;
+      printf("Number of elements of the array : ");
+
+      scanf("%d" , &n);
+
+      printf("Elements of the array : ");
+      
+      for(int  i = 0 ; i < n ; i += 1){
+            scanf("%d" , &a[i]);
+      }
+
+      int secondLargestElement = FindSecondLargestElement(a , n);
+
+      if(secondLargestElement == -1){
+           printf("The input is not valid\n");
+      }else{
+            printf("The second largest element is : %d\n" , secondLargestElement);
+      }
+
+      return 0 ;
+}
